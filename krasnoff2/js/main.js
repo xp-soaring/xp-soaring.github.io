06cb363eb869fca08e38802cccbb0cad5b60f4f2
Ian Lewis <ijl20@cam.ac.uk>
2022-09-30 12:46:10 +0100

    krasnoff2 WIP

--- krasnoff2/js/main.js
+++ krasnoff2/js/main.js
@@ -26,6 +26,17 @@ class Main {
         this.set_glider('asw28');
 
         this.tasks.update_page(this.main_tasks_el);
+
+        this.clock = new Date();
+        this.clock.setHours(12,0,0);
+
+        this.UPDATE_INTERVAL_MS = 100;  // when flying, update every 100 ms.
+        this.UPDATE_SPEED = 20;         // run simulation at 20 times real speed
+
+        this.timer = null; // We will use for setInterval timer
+
+        this.launch_el = document.getElementById("launch");
+        this.clock_el = document.getElementById("clock");
     }
 
     get_config(d) {
@@ -60,6 +71,35 @@ class Main {
         this.task.update_page(this.fly_message_el);
         this.weather.set_weather(this.task.settings.weather_id);
         this.weather.update_page(this.fly_weather_el);
+    }
+
+    // ******************************************************
+    // Flight
+    // ******************************************************
+
+    launch() {
+        let parent = this;
+        console.log("launch");
+
+        if (this.launch_el.value == "Launch" || this.launch_el.value == "Continue") {
+            parent.timer = window.setInterval( () => { parent.update(parent); }, parent.UPDATE_INTERVAL_MS );
+            this.launch_el.value = "Pause";
+        } else if (this.launch_el.value == "Pause") {
+            clearInterval(parent.timer);
+            this.launch_el.value = "Continue";
+        }
+    }
+
+    update(parent) {
+        // Update clock time
+        this.UPDATE_TIME_DELTA_MS = parent.UPDATE_INTERVAL_MS * parent.UPDATE_SPEED;
+        parent.clock.setSeconds(parent.clock.getSeconds() +  this.UPDATE_TIME_DELTA_MS / 1000);
+
+        parent.update_clock(parent);
+    }
+
+    update_clock(parent) {
+        parent.clock_el.value = parent.clock.toLocaleTimeString();
     }
 
     // ******************************************************
