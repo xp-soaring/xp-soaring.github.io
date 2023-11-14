e029ed489f0e47440cb74f08ccd9920241da2527
Ian Lewis <ijl20@cam.ac.uk>
2023-11-14 14:21:15 +0000

    b21_task_planner 4.1.7 nb21_updates

---
+++ tasks/b21_task_planner/js/lib/b21_gliders.js
@@ -0,0 +1,77 @@
+
+var B21_GLIDERS = {
+    "AS-33": {
+        "NB21_ACFG": [ "128D63EA22585AA47C381FC8",
+                    "21735116C3548A2C1B61D69B"
+        ],
+        "NB21_FMCG": [ "1B0477A5B0103B6FBBB971A6"
+        ],
+        "MAX_WEIGHT_KG": 600
+    },
+    "JS3-18": {
+        "NB21_ACFG": [ "753B807E002326612ACFA3A3"
+        ],
+        "NB21_FMCG": [ "5395E12E6384EABE7B636C6F"
+        ],
+        "MAX_WEIGHT_KG": 600
+    },
+    "JS3-15": {
+        "NB21_ACFG": [ "14E993B8CAA8350EF404F92C",
+                    "A51D14335FA6C71DBF197FEB"
+        ],
+        "NB21_FMCG": [ "350C66E7F8C644AC30537D6B"
+        ],
+        "MAX_WEIGHT_KG": 525
+    },
+    "LS4": {
+        "NB21_ACFG": [ "A48A4EF4E2A9D96C843B4361",
+                    '067AD89CAA95B9D8F2104D3C'
+        ],
+        "NB21_FMCG": [ "2CF10E6AEDEF85C111F41401",
+                    '49297C175A997216905BE06D'
+        ],
+        "MAX_WEIGHT_KG": 525
+    },
+    "DG808S": {
+        "NB21_ACFG": [ "0D933FB14EC7C1FC5E9C50FF",
+                    '617D8901FFC57865966C1A42',
+                    'AD2A2DAE9489BE555A943A0B'
+        ],
+        "NB21_FMCG": [ "612014C72D2B9816C06CB778",
+                    '8BDD0288671E70B14A3F9944'
+        ],
+        "MAX_WEIGHT_KG": 600
+    },
+    "AS7": {
+        "NB21_ACFG": [ "0CF71B19CD5135728AE56972"
+        ],
+        "NB21_FMCG": [ "51EF38E151AAAFAA8D56AB13"
+        ],
+        "MAX_WEIGHT_KG": 484.5
+    },
+    "D2C": {
+        "NB21_ACFG": [ "3E0C44899BFAE022333DD031"
+        ],
+        "NB21_FMCG": [ "9423DEDB0380C1A9D482080A",
+                'F5F9FFE0AC73623470D0D39A'
+        ],
+        "MAX_WEIGHT_KG": 525
+    },
+    "Asobo_LS8": {
+        "NB21_ACFG": [ "3B42B4378BE570DF675D2363F",
+                    '913A154DD71DB358BC981B3A'
+        ],
+        "NB21_FMCG": [ "1101DA2C71E9B25D1363D936",
+                    'E9CC0A261EFB7FB1449287ED'
+        ],
+        "MAX_WEIGHT_KG": 575
+    },
+    "DGF": {
+        "NB21_ACFG": [ "CA728979D50E2D4E1B413891"
+        ],
+        "NB21_FMCG": [ "AEDA50561766217B28C244B7"
+        ],
+        "MAX_WEIGHT_KG": 750
+    }
+
+};
